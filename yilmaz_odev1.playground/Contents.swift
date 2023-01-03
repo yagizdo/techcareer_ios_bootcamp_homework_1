a7788743badbada9e6b6429048f990870d2eccf7
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2023-01-03 23:09:36 +0300

    Homework 1 initialized

---
+++ yilmaz_odev1.playground/Contents.swift
@@ -0,0 +1,96 @@
+import UIKit
+
+var city : String = "Istanbul"
+var country : String = "Turkey"
+var phoneNumber : Int = 055555555
+var postCode : Int = 38000
+var email : String = "homework_yilmaz@bootcamp.com"
+var job : String = "iOS Developer"
+var stockAmount : Int = 10
+var customerName : String = "Yilmaz"
+var balance : Int = 250
+var birthday : String = "28 January 1999"
+var salary : Int = 15000
+var marialStatus : String = "Single"
+var productReview : String = "Excellent!"
+var payDate : String = "23.01.2023"
+var pay : Int = 18500
+var orderQuantity : Int = 180
+var carModel : String = "Supra"
+var bookName : String = "Ulysess Moore : The Door to Time"
+var publishTime : String = "2004"
+var discountAmount : Double = 0.52
+var numberOfRooms : Int = 100
+var latitude : Int = 150
+var longitude : Int = 500
+var productName : String = "Elgato Stream Deck"
+var foodPrice : Int = 100
+var brand : String = "Bugatti"
+var songName : String = "The Night Begins to Shine"
+var videoTime : Int = 5000
+var productScore : Int = 400
+var imageName : String = "The Persistence of Memory"
+var fileFormat : String = "pdf"
+var color : String = "Blue"
+var colorCode : String = "#0000FF"
+var phoneModel : String = "iPhone 3GS"
+var screenSize : Int = 17 // inch type
+var weight : Int = 99
+var nationalDay : String = "19 May"
+var holiday : String = "Sunday"
+var bookingDate : String = "23 July"
+var streetName : String = "Sarsilmaz Sokak"
+var busLine : String = "94H"
+var remainingMinute : Int = 38
+var trackingcode : Int = 38696
+var couponTime : Int = 100
+var couponCode : String = "495DKGKD040"
+var billingAddress : String = "Istanbul/Turkey"
+
+print("City : \(city)")
+print("Country : \(country)")
+print("Phone Number : \(phoneNumber)")
+print("Post Code : \(postCode)")
+print("Email : \(email)")
+print("Job : \(job)")
+print("Stock Amount : \(stockAmount)")
+print("Customer Name : \(customerName)")
+print("Balance : \(balance)")
+print("Birthday : \(birthday)")
+print("Salary : \(salary)")
+print("Marial Status : \(marialStatus)")
+print("Product Review : \(productReview)")
+print("Pay Date : \(payDate)")
+print("Pay : \(pay)")
+print("Order Amount : \(orderQuantity)")
+print("Car Model : \(carModel)")
+print("Book Name : \(bookName)")
+print("Publish Time : \(publishTime)")
+print("Discount Amount : \(discountAmount)")
+print("Number of Rooms : \(numberOfRooms)")
+print("Latitude : \(latitude)")
+print("Longitude : \(longitude)")
+print("Product Name : \(productName)")
+print("Food Price : \(foodPrice)")
+print("Brand : \(brand)")
+print("Song Name : \(songName)")
+print("Video Time : \(videoTime)")
+print("Product Score : \(productScore)")
+print("Image Name : \(imageName)")
+print("File Format: \(fileFormat)")
+print("Color : \(color)")
+print("Color Code : \(colorCode)")
+print("Phone Model : \(phoneModel)")
+print("Screen Size : \(screenSize)")
+print("Weight : \(weight)")
+print("National Day : \(nationalDay)")
+print("Holiday : \(holiday)")
+print("Booking Date : \(bookingDate)")
+print("Street Name : \(streetName)")
+print("Bus Line: \(busLine)")
+print("Remaining Minute : \(remainingMinute)")
+print("Tracking Code : \(trackingcode)")
+print("Coupon Time : \(couponTime)")
+print("Coupon Code : \(couponCode)")
+print("Billing Adress : \(billingAddress)")
+
